cdb1eeb0d72ac3dc6af33ff4f5b34744279783cb
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2021-11-26 12:04:51 +0000

    update to App.JSX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = useState({
+  const [contact, setFullContact] = useState({
     fName: "",
-    lName: ""
+    lName: "",
+    email: ""
   });
 
   function handle_change(event) {
     const { value, name } = event.target;
 
-    setFullName((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value
-        };
-      }
+    setFullContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
     // console.log(value);
     // console.log(name);
@@ -28,20 +22,27 @@ function App() {
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
         <input
           name="fName"
           onChange={handle_change}
           placeholder="First Name"
-          value={fullName.fName}
+          value={contact.fName}
         />
         <input
           name="lName"
           onChange={handle_change}
           placeholder="Last Name"
-          value={fullName.lName}
+          value={contact.lName}
+        />
+        <input
+          name="email"
+          onChange={handle_change}
+          placeholder="Email"
+          value={contact.email}
         />
         <button>Submit</button>
       </form>
